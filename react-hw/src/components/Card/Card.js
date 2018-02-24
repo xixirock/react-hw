5f45039eeb8ff06c782eccf8d9fa6f04b5e01841
xixirock <jlin0402@gmail.com>
2018-02-24 11:33:27 -0800

    component Card made

---
+++ react-hw/src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+    <div
+            className = "cardStyle"
+            onclick = {() => props.setClicked(props.id)}>
+        <div className = "imgContainer">
+            <img
+                className = "img"
+                alt = {props.name}
+                src = {props.image}
+            />
+        </div>
+        <div className = "content">
+            {props.name}
+        </div>
+    </div>
+);
+
+
+export default Card;
